e0476e4ea854c59f5328e8ae2ed97f88107a5769
Prithvi Chauhan <pthakur.pt36@gmail.com>
2023-10-16 18:44:39 +0530

    Project updatd

--- untitled/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("This is my first jenkins project which is working fine in this enviroment with the jenkins");
-        System.out.println("Hurray Hurray !!!");
-
-    }
-}
