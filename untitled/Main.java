1ce58450ad3fcd6793ad7e6ea368a237ac17f81e
Prithvi Chauhan <pthakur.pt36@gmail.com>
2023-10-16 18:39:35 +0530

    Few lines added to update the code

--- untitled/Main.java
+++ untitled/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("This is my first jenkins project which is working fine in this enviroment with the jenkins");
+        System.out.println("Hurray Hurray !!!");
+
     }
 }
